e35b22895a942675a2a6dd7d661f2a28ae9e9d80
yorickburgers <yorickburgers@live.nl>
2024-10-05 16:20:12 +0200

    Opdracht 1 en 2 done

--- scripts/main.js
+++ scripts/main.js
@@ -71,5 +71,27 @@ const departments = {
     }
 }
 
-console.log(departments);
+/*console.log(departments);*/
 
+/* Opdracht 1 */
+console.log("De afdeling Sales heeft " + departments.sales.numberOfEmployees + " medewerkers");
+console.log(" Marketing is een leuke afdeling om in te werken. " + departments.marketing.description);
+console.log("De afdeling Customer Service heeft " + departments["customer-service"].numberOfEmployees + " medewerkers");
+console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager. " + departments.sales.jobs[1].description);
+
+/* Opdracht 2 */
+const userInput = prompt('Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]');
+console.log(userInput);
+switch (userInput) {
+    case "marketing":
+        console.log("Je koos " + userInput + ". " + departments.marketing.description);
+        break;
+    case "sales":
+        console.log("Je koos " + userInput + ". " + departments.sales.description);
+        break;
+    case "customer-service":
+        console.log("je koos " + userInput + ". " + departments["customer-service"].description);
+        break;
+    default:
+        console.error("Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.")
+}
